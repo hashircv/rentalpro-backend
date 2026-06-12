01a40cd6fe0c1322c9d3eaec82df6df62677dabf
hashir <hashircv917@gmail.com>
2026-06-12 16:59:01 +0530

    checking cors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   process.env.CLIENT_URL,
 ].filter(Boolean);
-
+console.log('CLIENT_URL:', process.env.CLIENT_URL);
+console.log('Allowed Origins:', allowedOrigins);
 app.use(
   cors({
     origin: (origin, callback) => {
